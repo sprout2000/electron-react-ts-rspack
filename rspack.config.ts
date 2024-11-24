23dc7303e356bcf7389ff6efa9c82973628024b7
sprout2000 <wassabie64@gmail.com>
2024-11-24 09:07:49 +0900

    first commit

---
+++ rspack.config.ts
@@ -0,0 +1,82 @@
+import type { Configuration } from "@rspack/cli";
+import { CssExtractRspackPlugin } from "@rspack/core";
+import HtmlWebpackPlugin from "html-webpack-plugin";
+
+const isDev = process.env.NODE_ENV === "development";
+
+const common: Configuration = {
+  mode: isDev ? "development" : "production",
+  externals: ["fsevents"],
+  resolve: {
+    extensions: [".js", ".ts", ".jsx", ".tsx", ".json"],
+  },
+  output: {
+    publicPath: "./",
+    assetModuleFilename: "assets/[name][ext]",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        exclude: /node_modules/,
+        loader: "builtin:swc-loader",
+        options: {
+          jsc: {
+            parser: {
+              syntax: "typescript",
+            },
+            transform: {
+              react: {
+                runtime: "automatic",
+              },
+            },
+          },
+        },
+      },
+      {
+        test: /\.css$/,
+        use: [CssExtractRspackPlugin.loader, "css-loader"],
+        type: "javascript/auto",
+      },
+      {
+        test: /\.(ico|png|svg|eot|woff?2?)$/,
+        type: "asset/resource",
+      },
+    ],
+  },
+  watch: isDev,
+  devtool: isDev ? "source-map" : false,
+};
+
+const main: Configuration = {
+  ...common,
+  target: "electron-main",
+  entry: {
+    main: "./src/main.ts",
+  },
+};
+
+const preload: Configuration = {
+  ...common,
+  target: "electron-preload",
+  entry: {
+    preload: "./src/preload.ts",
+  },
+};
+
+const renderer: Configuration = {
+  ...common,
+  target: "web",
+  entry: {
+    app: "./src/web/index.tsx",
+  },
+  plugins: [
+    new CssExtractRspackPlugin(),
+    new HtmlWebpackPlugin({
+      inject: "body",
+      template: "./src/web/index.html",
+    }),
+  ],
+};
+
+export default [main, preload, renderer];
---
+++ src/web/App.tsx
@@ -0,0 +1,15 @@
+import { useState } from "react";
+import "./App.css";
+
+export const App = () => {
+  const [count, setCount] = useState(0);
+
+  return (
+    <div className="container">
+      <h1>{count}</h1>
+      <button type="button" onClick={() => setCount((count) => count + 1)}>
+        Count
+      </button>
+    </div>
+  );
+};
